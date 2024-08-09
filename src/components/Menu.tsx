b2c1b9ef89cb2de8236a10f80a782c01db8ff04d
taalto <taalto@opentext.com>
2024-08-09 15:01:23 +0300

    Update Menu.tsx

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,19 +1,12 @@
-import { FormattedMessage } from 'react-intl';
 import menu from '../img/menu2.png';
 import './Menu.scss';
 
 export const Menu = () => {
   return (
     <section className='Menu'>
-      {/* <h1>
-        <FormattedMessage id='main.menu' />
-      </h1> */}
       <div className='Info-imageContainer'>
           <img src={menu} alt='Menu' />
         </div>
-      {/* <p className='subText'>
-        <FormattedMessage id='main.program.beingUpdated' />
-      </p> */}
     </section>
   );
 };
