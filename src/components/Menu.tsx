748507cf2ad87ce5e230e9d78edef39bd27d18ea
taalto <taalto@opentext.com>
2024-08-09 14:48:03 +0300

    Add menu

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,15 +1,19 @@
 import { FormattedMessage } from 'react-intl';
+import menu from '../img/menu2.png';
 import './Menu.scss';
 
 export const Menu = () => {
   return (
     <section className='Menu'>
-      <h1>
+      {/* <h1>
         <FormattedMessage id='main.menu' />
-      </h1>
-      <p className='subText'>
+      </h1> */}
+      <div className='Info-imageContainer'>
+          <img src={menu} alt='Menu' />
+        </div>
+      {/* <p className='subText'>
         <FormattedMessage id='main.program.beingUpdated' />
-      </p>
+      </p> */}
     </section>
   );
 };
